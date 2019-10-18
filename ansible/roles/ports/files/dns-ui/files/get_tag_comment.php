e9af853876c26425a97bb219836b2e1faf292b97
sipopo <sipopo@yandex.ru>
2019-10-18 12:23:43 +0300

    fix in comment

--- ansible/roles/ports/files/dns-ui/files/get_tag_comment.php
+++ ansible/roles/ports/files/dns-ui/files/get_tag_comment.php
@@ -3,6 +3,6 @@
 function get_tag_comment($login)
 {
     $shortname = explode('.', gethostname());
-    $tag = date("d.m.Y H:i:s")." ".$shortname[0]." \"WEB\" ".$login." :";
+    $tag = date("d.m.Y H:i:s")." ".$shortname[0]." \"WEB\" ".$login.": ";
     return $tag;
 }
